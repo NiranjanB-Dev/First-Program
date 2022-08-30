c919f4b967108b950d747c095d361fbb19f901a7
Niranjan Barbhai <74625344+NiranjanB-Dev@users.noreply.github.com>
2022-08-31 01:28:13 +0530

    Add files via upload

---
+++ First Program.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      User
+#
+# Created:     26/08/2022
+# Copyright:   (c) User 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+first_name = "Tony"
+last_name = "Stark"
+age = 51
+is_genious = True
+
+print(first_name + " " + last_name)
+print(age)
+print(is_genious)
+
